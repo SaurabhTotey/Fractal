47293fa4ed03a1da6542894e81412d91e8f565dd
SaurabhTotey <SaurabhTotey@gmail.com>
2020-06-10 19:12:30 -0600

    Basic vulkan initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#![allow(non_snake_case)]
+
+use vulkano::instance::Instance;
+use vulkano::instance::InstanceExtensions;
+use vulkano::instance::PhysicalDevice;
+use vulkano::device::Device;
+use vulkano::device::DeviceExtensions;
+use vulkano::device::Features;
+
 fn main() {
-    println!("Hello, world!");
+	let instance = Instance::new(None, &InstanceExtensions::none(), None).expect("Failed to create instance.");
+	let physicalDevice = PhysicalDevice::enumerate(&instance).next().expect("No physical device available.");
+	let queueFamily = physicalDevice.queue_families().find(|&q| q.supports_graphics()).expect("Couldn't find a graphical queue family.");
+	let (device, mut queues) = { Device::new(
+		physicalDevice,
+		&Features::none(),
+		&DeviceExtensions::none(),
+		[(queueFamily, 0.5)].iter().cloned()
+	).expect("Failed to create device.") };
+	let queue = queues.next().unwrap();
 }
