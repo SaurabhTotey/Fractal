9fccadd607c7b2cbc6a8d05143def95e885c9b9a
SaurabhTotey <SaurabhTotey@gmail.com>
2020-06-12 12:04:55 -0600

    Put shader in a separate file

--- src/main.rs
+++ src/main.rs
@@ -26,19 +26,7 @@ fn main() {
 	mod ComputeShader {
 		vulkano_shaders::shader!{
 	        ty: "compute",
-	        src: "
-#version 450
-
-layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
-
-layout(set = 0, binding = 0) buffer Data {
-    uint data[];
-} buf;
-
-void main() {
-    uint idx = gl_GlobalInvocationID.x;
-    buf.data[idx] *= 12;
-}"
+	        path: "src/ComputeShader.glsl"
 	    }
 	}
 	let dataToMultiply = 0 .. 65536;
