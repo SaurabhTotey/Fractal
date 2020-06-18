d9d77f5997a831ec97e8512f856fa3683d72795e
Saurabh Totey <SaurabhTotey@gmail.com>
2020-06-18 14:12:13 -0600

    Removed compute shaders and added graphics shaders

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 #![allow(non_snake_case)]
 
 use std::sync::Arc;
+use rand::Rng;
 use vulkano::instance::{Instance, PhysicalDevice, InstanceExtensions};
 use vulkano::device::{Device, Features, DeviceExtensions};
 use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
-use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer, DynamicState};
 use vulkano::sync::GpuFuture;
-use vulkano::pipeline::ComputePipeline;
-use vulkano::descriptor::PipelineLayoutAbstract;
-use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
+use vulkano::pipeline::GraphicsPipeline;
 use vulkano::image::{StorageImage, Dimensions};
 use vulkano::format::Format;
+use vulkano::framebuffer::{Framebuffer, Subpass};
 use image::{Rgba, ImageBuffer};
+use vulkano::pipeline::viewport::Viewport;
 
 fn main() {
 	let instance = Instance::new(None, &InstanceExtensions::none(), None).expect("Failed to create instance.");
@@ -25,25 +26,74 @@ fn main() {
 	).expect("Failed to create device.") };
 	let queue = queues.next().unwrap();
 
-	//Should draw a circle to an image
-	mod ComputeShader {
-		vulkano_shaders::shader!{
-	        ty: "compute",
-	        path: "src/shaders/CircleComputeShader.glsl"
-	    }
+	#[derive(Default, Copy, Clone)]
+	struct Vertex {
+		position: [f32; 2],
 	}
-	let shader = ComputeShader::Shader::load(device.clone()).expect("Failed to create shader module.");
-	let computePipeline = Arc::new(ComputePipeline::new(device.clone(), &shader.main_entry_point(), &()).expect("Failed to create compute pipeline."));
+	vulkano::impl_vertex!(Vertex, position);
+
+	let mut rng = rand::thread_rng();
+	let mut generateVertex = || Vertex { position: [rng.gen::<f32>() * 2.0 - 1.0, rng.gen::<f32>() * 2.0 - 1.0] };
+	let vertices: Vec<Vertex> = (0..3).map(|_| generateVertex()).collect();
+
 	let imageSize = 1024;
 	let image = StorageImage::new(device.clone(), Dimensions::Dim2d { width: imageSize, height: imageSize }, Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
-	let layout = computePipeline.layout().descriptor_set_layout(0).unwrap();
-	let descriptorSet = Arc::new(PersistentDescriptorSet::start(layout.clone()).add_image(image.clone()).unwrap().build().unwrap());
 	let buffer = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, (0 .. imageSize * imageSize * 4).map(|_| 0u8)).expect("Failed to create Buffer.");
-	let mut builder = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap();
-	builder.dispatch([imageSize / 8, imageSize / 8, 1], computePipeline.clone(), descriptorSet.clone(), ()).unwrap().copy_image_to_buffer(image.clone(), buffer.clone()).unwrap();
-	let commandBuffer = builder.build().unwrap();
-	commandBuffer.execute(queue.clone()).unwrap().then_signal_fence_and_flush().unwrap().wait(None).unwrap();
+
+	mod VertexShader { vulkano_shaders::shader!{ ty: "vertex", path: "src/shaders/SetPositionVertexShader.glsl" } }
+	mod FragmentShader { vulkano_shaders::shader!{ ty: "fragment", path: "src/shaders/ColorInsideFragmentShader.glsl" } }
+	let vertexShader = VertexShader::Shader::load(device.clone()).expect("Failed to load vertex shader.");
+	let fragmentShader = FragmentShader::Shader::load(device.clone()).expect("Failed to load fragment shader.");
+	let vertexBuffer = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, vertices.into_iter()).unwrap();
+	let renderPass = Arc::new(vulkano::single_pass_renderpass!(device.clone(),
+	    attachments: {
+	        color: {
+	            load: Clear,
+	            store: Store,
+	            format: Format::R8G8B8A8Unorm,
+	            samples: 1,
+	        }
+	    },
+	    pass: {
+	        color: [color],
+	        depth_stencil: {}
+	    }
+	).unwrap());
+	let framebuffer = Arc::new(Framebuffer::start(renderPass.clone()).add(image.clone()).unwrap().build().unwrap());
+
+	let dynamicState = DynamicState {
+		viewports: Some(vec![Viewport {
+			origin: [0.0, 0.0],
+			dimensions: [imageSize as f32, imageSize as f32],
+			depth_range: 0.0 .. 1.0,
+		}]),
+		.. DynamicState::none()
+	};
+
+	let pipeline = Arc::new(GraphicsPipeline::start()
+		// Defines what kind of vertex input is expected.
+		.vertex_input_single_buffer::<Vertex>()
+		// The vertex shader.
+		.vertex_shader(vertexShader.main_entry_point(), ())
+		// Defines the viewport (explanations below).
+		.viewports_dynamic_scissors_irrelevant(1)
+		// The fragment shader.
+		.fragment_shader(fragmentShader.main_entry_point(), ())
+		// This graphics pipeline object concerns the first pass of the render pass.
+		.render_pass(Subpass::from(renderPass.clone(), 0).unwrap())
+		// Now that everything is specified, we call `build`.
+		.build(device.clone())
+		.unwrap());
+
+	let mut builder = AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family()).unwrap();
+	builder
+		.begin_render_pass(framebuffer.clone(), false, vec![[0.0, 1.0, 0.0, 1.0].into()]).unwrap()
+		.draw(pipeline.clone(), &dynamicState, vertexBuffer.clone(), (), ()).unwrap()
+		.end_render_pass().unwrap()
+		.copy_image_to_buffer(image.clone(), buffer.clone()).unwrap();
+	builder.build().unwrap().execute(queue.clone()).unwrap().then_signal_fence_and_flush().unwrap().wait(None).unwrap();
+
 	let bufferContent = buffer.read().unwrap();
-	let image = ImageBuffer::<Rgba<u8>, _>::from_raw(imageSize, imageSize, &bufferContent[..]).unwrap();
-	image.save("output/Circle.png").unwrap();
+	let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &bufferContent[..]).unwrap();
+	image.save("output/TriangleTest.png").unwrap();
 }
